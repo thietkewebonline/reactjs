c888fc9a60839b5fb57eda6e95b89d05868ce963
Vui Nguyen <vui.nguyen@cofoundervp.com>
2017-11-24 14:48:32 +0700

    add new script

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
-var config = {
-   entry: './main.js',
-   output: {
-      path:'/',
-      filename: 'index.js',
-   },
-   devServer: {
-      inline: true,
-      port: 8080
-   },
-   module: {
-      loaders: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-               presets: ['es2015', 'react']
+const path = require("path");
+const config = {
+    entry: './main.js',
+    output: {
+        // path:'/',
+        path: path.join(__dirname, "/"),
+        filename: 'index.js',
+    },
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
             }
-         }
-      ]
-   }
+        ]
+    }
 }
 module.exports = config;
