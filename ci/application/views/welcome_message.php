a7182c66242e5f640df90483b1bf53a467af00f3
dinhhieu67 <dinhhieu67@gmail.com>
2017-12-06 16:01:47 +0700

    edit view test

--- ci/application/views/welcome_message.php
+++ ci/application/views/welcome_message.php
@@ -37,7 +37,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	code {
 		font-family: Consolas, Monaco, Courier New, Courier, monospace;
 		font-size: 12px;
-		background-color: #f9f9f9;
+		background-color: #f9f9fs9;
 		border: 1px solid #D0D0D0;
 		color: #002166;
 		display: block;
@@ -68,21 +68,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	hello 2 kk gfjgkfgjfk kakakak
 </div>
 
 </body>
